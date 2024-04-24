0dab3491f0656b1b4ca817536393ea868c3514d2
mblaqsj1015 <mblaqsj1015@naver.com>
2024-04-24 11:10:15 +0900

    ✨ feat: create schedule requestDto

---
+++ backend/src/main/java/space/habitz/api/domain/schedule/dto/ScheduleRequestDto.java
@@ -0,0 +1,37 @@
+package space.habitz.api.domain.schedule.dto;
+
+import lombok.Builder;
+import space.habitz.api.domain.schedule.entity.Schedule;
+
+import java.time.LocalDate;
+
+@Builder
+public record ScheduleRequestDto(
+	String title,
+	String content,
+	String emoji,
+	String childUUID,
+	LocalDate startDate,
+	LocalDate endDate,
+	Boolean [] weekDays, // size 7
+	Integer point
+	) {
+
+	public Schedule toEntity(){
+		return Schedule.builder()
+			.title(title)
+			.content(content)
+			.emoji(emoji)
+			.startDate(startDate)
+			.endDate(endDate)
+			.monday(weekDays[0])
+			.tuesday(weekDays[1])
+			.wednesday(weekDays[2])
+			.thursday(weekDays[3])
+			.friday(weekDays[4])
+			.saturday(weekDays[5])
+			.sunday(weekDays[6])
+			.point(point)
+			.build();
+	}
+}
