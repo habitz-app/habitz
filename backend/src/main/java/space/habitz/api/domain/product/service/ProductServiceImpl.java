8858bce05f4602134b0f4a175f4260e60be03f67
yhw991228 <yhw991228@ajou.ac.kr>
2024-04-24 16:46:47 +0900

    ✨ feat: add service for product single find

--- backend/src/main/java/space/habitz/api/domain/product/service/ProductServiceImpl.java
+++ backend/src/main/java/space/habitz/api/domain/product/service/ProductServiceImpl.java
@@ -1,2 +1,20 @@
-package space.habitz.api.domain.product.service;public class ProductServiceImpl {
+package space.habitz.api.domain.product.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import space.habitz.api.domain.product.domain.Product;
+import space.habitz.api.domain.product.repository.ProductRepository;
+
+@RequiredArgsConstructor
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	private final ProductRepository productRepository;
+
+	@Override
+	public Product getProductDetail(Long id) {
+		return productRepository.findProductById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Product not found"));
+	}
 }
