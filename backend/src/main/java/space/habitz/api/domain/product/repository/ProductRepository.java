58ed2434f3724cd3e63937e3abc7050d8a53db9b
yhw991228 <yhw991228@gmail.com>
2024-04-25 17:42:48 +0900

    ✨ feat: add list product repository
    Change-Id: I00424d8a86a8b33c3eab9cbe274d5f238a66b3c6

--- backend/src/main/java/space/habitz/api/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/space/habitz/api/domain/product/repository/ProductRepository.java
@@ -2,12 +2,16 @@ package space.habitz.api.domain.product.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import lombok.NonNull;
 import space.habitz.api.domain.product.domain.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	Optional<Product> findProductById(Long id);
+	@NonNull Page<Product> findAll(@NonNull Pageable pageable);
 }
