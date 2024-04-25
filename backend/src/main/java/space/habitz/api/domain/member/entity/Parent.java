4c57aa639ac61075bcc371fa714ae94cc51d89a0
조승우 <kjsw12@naver.com>
2024-04-25 14:47:53 +0900

    ✏️ fix: Fix the Member Domain Entity
    Change-Id: I68a8baae8d08df91fc7edc0a181ccbf768492005

--- backend/src/main/java/space/habitz/api/domain/member/entity/Parent.java
+++ backend/src/main/java/space/habitz/api/domain/member/entity/Parent.java
@@ -2,6 +2,11 @@ package space.habitz.api.domain.member.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,8 +16,18 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@DiscriminatorValue("Parent")
-@PrimaryKeyJoinColumn(name = "member_id")
-public class Parent extends Member {
+public class Parent {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	Member member;
+
 	private Long point;
+
+	public Parent(Member member, Long point) {
+		this.member = member;
+		this.point = point;
+	}
 }
---
+++ backend/src/main/java/space/habitz/api/domain/member/entity/Role.java
@@ -0,0 +1,29 @@
+package space.habitz.api.domain.member.entity;
+
+
+import java.util.Arrays;
+
+public enum Role {
+    PARENT("ROLE_PARENT"),
+    CHILD("ROLE_CHILD"),
+    ADMIN("ROLE_ADMIN"),
+    GUEST("ROLE_GUEST");
+
+    private final String role;
+
+    public String getRoleName() {
+        return this.role.substring(5);
+    }
+
+    public static Role findEnum(String target) {
+        return Arrays.stream(values())
+                .filter(t -> t.getRoleName().equals(target))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    Role(String role) {
+        this.role = role;
+    }
+}
+
