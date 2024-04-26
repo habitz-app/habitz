a7b45f727c94884e648f28f5165fe0a84364df90
yhw991228 <yhw991228@gmail.com>
2024-04-26 18:05:31 +0900

    ✨ feat: add entity for pay
    Change-Id: I3678b3ff336602bdc6b348d2e9edcf3accdca307

---
+++ backend/src/main/java/space/habitz/api/domain/point/entity/FamilyPointHistory.java
@@ -0,0 +1,21 @@
+package space.habitz.api.domain.point.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class FamilyPointHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "remain_point")
+	private int remainPoint;
+	@Column(name = "pay_point")
+	private int payPoint;
+	@Column(name = "charge_id") // 부모 포인트 결제 내역 id
+	private Long chargeId;
+	@Column(name = "payment_id") // 자녀 미션 승인 id
+	private Long paymentId;
+
+}
