c3f7f49effd36167cd04a78058d1e81c4be6f103
mblaqsj1015 <tpwjd29@gmail.com>
2024-04-26 15:50:04 +0900

    ✨ feat: add ScheduleService
    Change-Id: I8c63accb9b46ef280e547988fbf0dd9a6bf56dae

--- backend/src/main/java/space/habitz/api/domain/schedule/service/ScheduleService.java
+++ backend/src/main/java/space/habitz/api/domain/schedule/service/ScheduleService.java
@@ -4,9 +4,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import space.habitz.api.domain.member.entity.Member;
+import space.habitz.api.domain.schedule.dto.ScheduleDto;
 import space.habitz.api.domain.schedule.dto.ScheduleRequestDto;
 import space.habitz.api.domain.schedule.entity.Schedule;
 import space.habitz.api.domain.schedule.repository.ScheduleRepository;
+import space.habitz.api.global.exception.CustomErrorException;
+import space.habitz.api.global.exception.ErrorCode;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +22,16 @@ public class ScheduleService {
 
 		Schedule schedule = scheduleRequestDto.toEntity();
 		scheduleRepository.save(schedule);
+	}
+
+	/**
+	 * 일정 상세 조회
+	 *
+	 * @param member     로그인한 사용자 정보
+	 * @param scheduleId 일정 ID
+	 */
+	public ScheduleDto getScheduleDetail(Member member, Long scheduleId) {
+		Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(() -> new CustomErrorException(ErrorCode.SCHEDULE_NOT_FOUND));
+		return ScheduleDto.of(schedule);
 	}
 }
