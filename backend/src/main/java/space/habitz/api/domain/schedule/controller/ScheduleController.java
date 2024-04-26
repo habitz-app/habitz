eaf4cda991bad6f41b6daf88d103d137b0db0597
mblaqsj1015 <mblaqsj1015@naver.com>
2024-04-26 15:50:04 +0900

    ✨ feat: add ScheduleService
    Change-Id: I8c63accb9b46ef280e547988fbf0dd9a6bf56dae

--- backend/src/main/java/space/habitz/api/domain/schedule/controller/ScheduleController.java
+++ backend/src/main/java/space/habitz/api/domain/schedule/controller/ScheduleController.java
@@ -1,19 +1,37 @@
 package space.habitz.api.domain.schedule.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import space.habitz.api.domain.member.entity.Member;
+import space.habitz.api.domain.schedule.dto.ScheduleDto;
+import space.habitz.api.domain.schedule.service.ScheduleService;
 import space.habitz.api.global.response.ResponseData;
 
+@Tag(name = "Schedule", description = "일정 관련 API")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/schedule")
 public class ScheduleController {
+
+	private final ScheduleService scheduleService;
 
 	@PostMapping
 	public ResponseData<Integer> createSchedule(@RequestHeader String auth) {
 		return ResponseData.success(1);
+	}
+
+
+	@Operation(
+		summary = "일정 상세 조회",
+		description = "일정 상세 정보를 조회합니다."
+	)
+	@GetMapping("/{scheduleId}")
+	public ResponseData<ScheduleDto> getScheduleDetail(@AuthenticationPrincipal Member member, @PathVariable Long scheduleId) {
+		ScheduleDto scheduleDto = scheduleService.getScheduleDetail(member, scheduleId);
+		return ResponseData.success(scheduleDto);
 	}
 
 }
