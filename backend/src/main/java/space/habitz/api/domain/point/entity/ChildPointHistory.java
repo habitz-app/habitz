9f5d49cbf130cc1e46b6e51f239f9cac3ca9971a
yhw991228 <yhw991228@gmail.com>
2024-04-26 18:05:31 +0900

    ✨ feat: add entity for pay
    Change-Id: I3678b3ff336602bdc6b348d2e9edcf3accdca307

--- backend/src/main/java/space/habitz/api/domain/point/entity/ChildPointHistory.java
+++ backend/src/main/java/space/habitz/api/domain/point/entity/ChildPointHistory.java
@@ -1,9 +1,12 @@
 package space.habitz.api.domain.point.entity;
 
+import java.sql.Timestamp;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import space.habitz.api.domain.member.entity.Child;
 
+@Getter
 @Entity
 @Table(name = "child_point_history")
 public class ChildPointHistory {
@@ -16,5 +19,15 @@ public class ChildPointHistory {
 	@JoinColumn(name = "child_id")
 	private Child child;
 
+	@Column(name = "point")
+	private Long point;
+	@Column(name = "total_point")
+	private Long totalPoint;
+	@Column(name = "created_at")
+	private Timestamp createdAt;
+	@Column(name = "mission_id")
+	private Long missionId;
+	@Column(name = "payment_id")
+	private Long paymentId;
 
 }
