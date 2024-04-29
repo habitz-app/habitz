6e26e204b4f308127f72a35339222adfcd7c0425
kimgiraffe <kimgiraffe@naver.com>
2024-04-29 15:42:58 +0900

    ✨ feat: add chargeButton component and its stories
    Change-Id: I5557aac8d560e6cbf9f4339551ca9f718d129991

---
+++ frontend/src/components/charge/ChargeButton.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ChargeButton from './ChargeButton';
+
+export default {
+  title: 'ChargeButton',
+  component: ChargeButton,
+} as Meta;
+
+type Story = StoryObj<typeof ChargeButton>;
+
+export const Default: Story = {};
