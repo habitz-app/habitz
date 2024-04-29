551ab652222c1526aa8d9c3899dc080d5c10e192
조승우 <kjsw12@naver.com>
2024-04-29 17:47:47 +0900

    ✨ feat: add Dummy Member Controller
    Change-Id: I9ef72ec3c89732b1b526566b7e5ce36214c1f881

---
+++ backend/src/main/java/space/habitz/api/domain/test/dto/DummyMemberLoginRequestDto.java
@@ -0,0 +1,13 @@
+package space.habitz.api.domain.test.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class DummyMemberLoginRequestDto {
+	private Long memberId;
+}
