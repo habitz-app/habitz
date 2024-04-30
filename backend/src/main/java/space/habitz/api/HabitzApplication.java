6c29b5bc2ab5249ceaee6161dd4ac4b4d5363643
mblaqsj1015 <mblaqsj1015@naver.com>
2024-05-01 04:08:59 +0900

    ✨ feat: setting TimeZone Asia/Seoul on Sring Boot Application
    - Scheduling 할 때 시간 맞추기 위해 애플리케이션에 TimeZone  설정
    - Scheulder 실행을 위한 어노테이션 추가
    
    Change-Id: I376e063593aa48ac6f28b5dde45cf2124da63f73

--- backend/src/main/java/space/habitz/api/HabitzApplication.java
+++ backend/src/main/java/space/habitz/api/HabitzApplication.java
@@ -2,12 +2,16 @@ package space.habitz.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class HabitzApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(HabitzApplication.class, args);
 	}
 
