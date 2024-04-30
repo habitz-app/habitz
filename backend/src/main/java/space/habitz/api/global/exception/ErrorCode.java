ddbc27fbba12d1f37d738e9efd6dcd0d439dd3f8
mblaqsj1015 <tpwjd29@gmail.com>
2024-04-30 10:18:09 +0900

    ✨ feat: add ErrorCode for error Information
    - 오류에 대한 명세를 Enum 을 기준으로 추가합니다.
    
    Change-Id: I6fea294cd2de724cec64d6d631e750e6f8540a1f

--- backend/src/main/java/space/habitz/api/global/exception/ErrorCode.java
+++ backend/src/main/java/space/habitz/api/global/exception/ErrorCode.java
@@ -5,16 +5,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-	/**
-	 * Schedule
-	 */
-	SCHEDULE_NOT_FOUND(NOT_FOUND, "Schedule Not Found");
+    /**
+     * Member
+     */
+    MEMBER_NOT_FOUND(NOT_FOUND, "찾을 수 없는 회원입니다."),
 
-	public final HttpStatus httpStatus;
-	public final String errorMessage;
+    /**
+     * Child, Parent Authority
+     */
+    CHILD_CAN_NOT_CREATE_SCHEDULE(UNAUTHORIZED, "아이는 스케줄을 생성할 수 없습니다."),
+
+    /**
+     * Schedule
+     */
+    SCHEDULE_NOT_FOUND(NOT_FOUND, "찾을 수 없는 일정입니다.");
+
+    public final HttpStatus httpStatus;
+    public final String errorMessage;
 }
