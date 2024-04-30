075cf522f7ea182b729373defb95e03aec6e8c9d
mblaqsj1015 <tpwjd29@gmail.com>
2024-04-30 23:17:49 +0900

    ✨ feat: create schedule api
    - 일정 생성 API 개발
    
    Change-Id: Idef4403813149fce1eccdb299c43e5028b6f4c94

--- backend/src/main/java/space/habitz/api/domain/schedule/dto/ScheduleRequestDto.java
+++ backend/src/main/java/space/habitz/api/domain/schedule/dto/ScheduleRequestDto.java
@@ -1,37 +1,52 @@
 package space.habitz.api.domain.schedule.dto;
 
 import lombok.Builder;
+import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.schedule.entity.Schedule;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Builder
 public record ScheduleRequestDto(
-	String title,
-	String content,
-	String emoji,
-	String childUUID,
-	LocalDate startDate,
-	LocalDate endDate,
-	Boolean [] weekDays, // size 7
-	Integer point
-	) {
+        String title,
+        String content,
+        String emoji,
+        String childUUID,
+        LocalDate startDate,
+        LocalDate endDate,
+        Boolean[] weekDays, // size 7
+        Integer point
+) {
+    public Schedule toEntity(Member parent, Member child) {
 
-	public Schedule toEntity(){
-		return Schedule.builder()
-			.title(title)
-			.content(content)
-			.emoji(emoji)
-			.startDate(startDate)
-			.endDate(endDate)
-			.monday(weekDays[0])
-			.tuesday(weekDays[1])
-			.wednesday(weekDays[2])
-			.thursday(weekDays[3])
-			.friday(weekDays[4])
-			.saturday(weekDays[5])
-			.sunday(weekDays[6])
-			.point(point)
-			.build();
-	}
+        boolean isRepeatable = !startDate.equals(endDate); // 반복 여부
+
+        // 단일 반복이라면 해당 date에만 요일 true
+        if (!isRepeatable) {
+            DayOfWeek dayOfWeek = startDate.getDayOfWeek();
+            Arrays.fill(weekDays, Boolean.FALSE);
+            weekDays[dayOfWeek.getValue() - 1] = Boolean.TRUE;
+        }
+
+        return Schedule.builder()
+                .title(title)
+                .content(content)
+                .emoji(emoji)
+                .point(point)
+                .parent(parent)
+                .child(child)
+                .startDate(startDate)
+                .endDate(endDate)
+                .monday(weekDays[0])
+                .tuesday(weekDays[1])
+                .wednesday(weekDays[2])
+                .thursday(weekDays[3])
+                .friday(weekDays[4])
+                .saturday(weekDays[5])
+                .sunday(weekDays[6])
+                .repeatable(isRepeatable)
+                .build();
+    }
 }
