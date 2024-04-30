a67e1677f45a16db9313b3c606cb322d844ff3d2
mblaqsj1015 <mblaqsj1015@naver.com>
2024-04-30 17:58:12 +0900

    ♻️ refactor: change GlobalExceptionHandler for returning by error message
    - globalExceptionHandler에 logger에는 원인이 찍히지만, error Message에 결과 값을 반환한다.
    
    Change-Id: Ifa4f91b54967913c7c8c5489848fe6cb19caf86a

--- backend/src/main/java/space/habitz/api/global/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/space/habitz/api/global/handler/GlobalExceptionHandler.java
@@ -14,39 +14,39 @@ import space.habitz.api.global.response.ResponseData;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
-		log.error("GlobalExceptionHandler: " + e.getMessage());
-		return new ResponseEntity<>(ResponseData.error(), HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error("GlobalExceptionHandler: " + e.getMessage());
+        return new ResponseEntity<>(ResponseData.error(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(NullPointerException.class)
-	public ResponseEntity<?> handleNullPointerException(NullPointerException e) {
-		log.error("GlobalExceptionHandler: " + e.getMessage());
-		return new ResponseEntity<>(ResponseData.error(), HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<?> handleNullPointerException(NullPointerException e) {
+        log.error("GlobalExceptionHandler: " + e.getMessage());
+        return new ResponseEntity<>(ResponseData.error(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handleException(Exception e) {
-		log.error("GlobalExceptionHandler: " + e.getMessage());
-		return new ResponseEntity<>(ResponseData.error(), HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error("GlobalExceptionHandler: " + e.getMessage());
+        return new ResponseEntity<>(ResponseData.error(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(ExpiredJwtException.class)
-	public ResponseEntity<?> handleExpiredJwtException(Exception e) {
-		log.error("GlobalExceptionHandler: " + e.getMessage());
-		return new ResponseEntity<>(ResponseData.error("401"), HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<?> handleExpiredJwtException(Exception e) {
+        log.error("GlobalExceptionHandler: " + e.getMessage());
+        return new ResponseEntity<>(ResponseData.error(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		log.error("GlobalExceptionHandler: " + e.getMessage());
-		return new ResponseEntity<>(ResponseData.error("401"), HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("GlobalExceptionHandler: " + e.getMessage());
+        return new ResponseEntity<>(ResponseData.error("401"), HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(AccessDeniedException.class)
-	public ResponseEntity<?> AccessDeniedException(AccessDeniedException e) {
-		log.error("GlobalExceptionHandler: " + e.getMessage());
-		return new ResponseEntity<>(ResponseData.unauthorized(), HttpStatus.FORBIDDEN);
-	}
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> AccessDeniedException(AccessDeniedException e) {
+        log.error("GlobalExceptionHandler: " + e.getMessage());
+        return new ResponseEntity<>(ResponseData.unauthorized(), HttpStatus.FORBIDDEN);
+    }
 }
