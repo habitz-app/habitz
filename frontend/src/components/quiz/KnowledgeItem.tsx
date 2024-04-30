3b69f9a4f206de8bd20ac2878ae8a4a6586aa663
kimgiraffe <kimgiraffe@naver.com>
2024-04-30 15:57:36 +0900

    ✨ feat: add KnowledgeItem component and stories
    Change-Id: I8388d9bc672a7453c7aa64c9f8619f0d98b415fe

---
+++ frontend/src/components/quiz/KnowledgeItem.tsx
@@ -0,0 +1,76 @@
+import { css } from 'styled-system/css';
+import { heartOutline } from 'ionicons/icons';
+import { IonIcon } from '@ionic/react';
+import { Button } from '@/components/ui/button';
+const KnowledgeItem = ({
+  title,
+  contents,
+}: {
+  title: string;
+  contents: string;
+}) => {
+  return (
+    <article
+      className={css({
+        my: '0.625rem',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '0.625rem',
+        borderBottom: `1px solid rgba(115, 115, 115, 0.22)`,
+      })}
+    >
+      <div
+        className={css({
+          display: 'flex',
+          justifyContent: 'space-between',
+          w: 'full',
+        })}
+      >
+        <div
+          className={css({
+            flexGrow: 1,
+          })}
+        >
+          <span
+            className={css({
+              textStyle: 'label1.normal.bold',
+              color: 'label.normal',
+              wordBreak: 'break-word',
+            })}
+          >
+            {title}
+          </span>
+          <p
+            className={css({
+              textStyle: 'caption1.medium',
+              color: 'label.alternative',
+              wordBreak: 'break-word',
+              mb: '0.625rem',
+            })}
+          >
+            {contents}
+          </p>
+        </div>
+        <Button
+          m={2}
+          color={'label.alternative'}
+          variant={'link'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignSelf={'center'}
+          alignItems={'center'}
+        >
+          <IonIcon
+            icon={heartOutline}
+            className={css({
+              w: '2.5rem',
+              h: '2.5rem',
+            })}
+          />
+        </Button>
+      </div>
+    </article>
+  );
+};
+
+export default KnowledgeItem;
