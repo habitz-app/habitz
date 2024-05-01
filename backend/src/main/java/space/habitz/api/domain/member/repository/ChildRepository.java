cfcf5d4d5cdfd9b9840959991d84b6225a22769d
용희원 <yhw991228@gmail.com>
2024-05-02 01:21:17 +0900

    ✨ feat: find childid by memeber id
    Change-Id: Ia237f228d4a94635ae15599c21b506d8ba20ad5b

--- backend/src/main/java/space/habitz/api/domain/member/repository/ChildRepository.java
+++ backend/src/main/java/space/habitz/api/domain/member/repository/ChildRepository.java
@@ -1,10 +1,13 @@
 package space.habitz.api.domain.member.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import space.habitz.api.domain.member.entity.Child;
-
 import java.util.Optional;
 
-public interface ChildRepository extends JpaRepository<Child, Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import space.habitz.api.domain.member.entity.Child;
+
+public interface ChildRepository extends JpaRepository<Child, Long> {
 	Optional<Child> findById_AndMember_Family_Id(Long id, String familyId);
+
+	Child findByMember_Id(Long memberId);
 }
