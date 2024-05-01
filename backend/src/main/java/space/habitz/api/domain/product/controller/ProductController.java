22f5169586d0a03a60940ecadcf55d363af79c3d
용희원 <yhw991228@gmail.com>
2024-05-01 20:59:12 +0900

    ✨ feat: add controller for ban product
    Change-Id: I3e267c9f1a8ae3268fb122f1136d8a37fe7dd3f9

--- backend/src/main/java/space/habitz/api/domain/product/controller/ProductController.java
+++ backend/src/main/java/space/habitz/api/domain/product/controller/ProductController.java
@@ -3,19 +3,20 @@ package space.habitz.api.domain.product.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
 
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import space.habitz.api.domain.product.dto.ProductBanDto;
 import space.habitz.api.domain.product.dto.ProductInfoDto;
+import space.habitz.api.domain.product.entity.BannedProduct;
 import space.habitz.api.domain.product.service.ProductService;
 import space.habitz.api.global.response.ResponseData;
+import space.habitz.api.domain.member.entity.Member;
 
 @Slf4j
 @RestController
@@ -38,10 +39,17 @@ public class ProductController {
 	}
 
 	@ApiResponse(description = "제한한 상품 조회")
-	@GetMapping("/banned-product-list/{childId}")
+	@GetMapping("/banned-product/list/{childId}")
 	public ResponseData<Page<ProductInfoDto>> getBannedProductList(
 		Pageable pageable,
 		@Parameter(description = "상품 ID", required = true) @PathVariable("childId") long childId) {
 		return new ResponseData<>("success", "제한한 상품 조회 성공", productService.getBannedProductInfo(childId, pageable));
+	}
+
+	@ApiResponse(description = "상품 제한하기")
+	@PostMapping("/banned-product/restrict")
+	public ResponseData<BannedProduct> getBannedProductList(@AuthenticationPrincipal Member member,
+															@RequestBody ProductBanDto productBanDto) {
+		return new ResponseData<>("success", "상품 제한 성공", productService.setBanProduct(member, productBanDto.getProductId(), productBanDto.getChildId()));
 	}
 }
