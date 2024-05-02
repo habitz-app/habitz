45fe780bd20ced13883b51b6904175f2397d3c97
조승우 <kjsw12@naver.com>
2024-05-02 16:06:42 +0900

    ✨ feat: implement Get Family Invite Code
    Change-Id: I0ae34f3d936d4cca05b156fd9c22293ffaccc576

---
+++ backend/src/main/java/space/habitz/api/domain/member/dto/MemberInviteCodeResponse.java
@@ -0,0 +1,8 @@
+package space.habitz.api.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MemberInviteCodeResponse {
+    private String inviteCode;
+}
---
+++ backend/src/main/java/space/habitz/api/domain/member/service/FamilyService.java
@@ -0,0 +1,8 @@
+package space.habitz.api.domain.member.service;
+
+import space.habitz.api.domain.member.dto.MemberInviteCodeResponse;
+
+
+public interface FamilyService {
+	MemberInviteCodeResponse getInviteCode();
+}
