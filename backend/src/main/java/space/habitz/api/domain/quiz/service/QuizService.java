f8c6177688c108444d8d5844ee149092c46c04c8
yhw991228 <yhw991228@gmail.com>
2024-05-02 13:01:35 +0900

    ✨ feat: add service for quiz history
    Change-Id: I490351a81d50b5a817ddbba0a771419467c5bbb0

--- backend/src/main/java/space/habitz/api/domain/quiz/service/QuizService.java
+++ backend/src/main/java/space/habitz/api/domain/quiz/service/QuizService.java
@@ -2,8 +2,10 @@ package space.habitz.api.domain.quiz.service;
 
 import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.quiz.dto.QuizHistoryDto;
+import space.habitz.api.domain.quiz.dto.QuizHistoryInfoDto;
 
 public interface QuizService {
 
 	QuizHistoryDto getTodayQuiz(Member member);
+	QuizHistoryDto solveQuiz(Member member, String answer);
 }
