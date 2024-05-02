3690ab96a73794d3d7d4480580d5aaf304e838d2
yhw991228 <yhw991228@gmail.com>
2024-05-02 13:02:02 +0900

    ✨ feat: add feature for quiz solve
    Change-Id: Id144030823c44e539e8f264bdb59923ac904f1a0

--- backend/src/main/java/space/habitz/api/domain/quiz/controller/QuizController.java
+++ backend/src/main/java/space/habitz/api/domain/quiz/controller/QuizController.java
@@ -2,6 +2,8 @@ package space.habitz.api.domain.quiz.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.member.entity.Role;
 import space.habitz.api.domain.quiz.dto.QuizHistoryDto;
+import space.habitz.api.domain.quiz.dto.QuizSolveInfoDto;
 import space.habitz.api.domain.quiz.service.QuizService;
 import space.habitz.api.global.response.ResponseData;
 
@@ -32,5 +35,18 @@ public class QuizController {
 		return new ResponseData<>("success", "퀴즈 조회 성공", quizService.getTodayQuiz(member));
 
 	}
+
+	@ApiResponse(description = "퀴즈 풀기")
+	@PostMapping("/solve-quiz")
+	public ResponseData<QuizHistoryDto> solveQuiz(@AuthenticationPrincipal Member member,
+		@RequestBody QuizSolveInfoDto quizSolveInfoDto) {
+		if (member.getRole() != Role.CHILD) {
+			return new ResponseData<>("fail", "어린이만 접근 가능한 페이지입니다.", null);
+		}
+
+		return new ResponseData<>("success", "퀴즈 풀기 성공", quizService.solveQuiz(member, quizSolveInfoDto.getAnswer()));
+	}
+
+
 
 }
