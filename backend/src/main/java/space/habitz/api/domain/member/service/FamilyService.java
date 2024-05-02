7bf9cd66234960e94b64444431b8cebb81ccb29b
조승우 <kjsw12@naver.com>
2024-05-02 16:32:53 +0900

    ✨ feat: Implement get Family MemberList
    Change-Id: I1150e0edc34a5faa93fdd349721de586f71e0989

--- backend/src/main/java/space/habitz/api/domain/member/service/FamilyService.java
+++ backend/src/main/java/space/habitz/api/domain/member/service/FamilyService.java
@@ -1,8 +1,13 @@
 package space.habitz.api.domain.member.service;
 
+import space.habitz.api.domain.member.dto.FamilyListResponseDto;
 import space.habitz.api.domain.member.dto.MemberInviteCodeResponse;
+
+import java.util.List;
 
 
 public interface FamilyService {
 	MemberInviteCodeResponse getInviteCode();
+
+	List<FamilyListResponseDto> getFamilyList();
 }
