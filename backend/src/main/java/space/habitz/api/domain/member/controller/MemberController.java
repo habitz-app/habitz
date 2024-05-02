952848bd5b2b5aad7bd88e33af37cd7a97e4d025
조승우 <kjsw12@naver.com>
2024-05-02 13:47:20 +0900

    ✨ feat: implement Member Join
    Change-Id: Id8c3525436a8784bea753fbd111b4fe33b5e2105

--- backend/src/main/java/space/habitz/api/domain/member/controller/MemberController.java
+++ backend/src/main/java/space/habitz/api/domain/member/controller/MemberController.java
@@ -1,15 +1,13 @@
 package space.habitz.api.domain.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import space.habitz.api.domain.member.dto.*;
+import space.habitz.api.domain.member.entity.*;
 import space.habitz.api.domain.member.service.MemberService;
 import space.habitz.api.global.response.ResponseData;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @RestController
 @RequestMapping("/api/v1/member")
@@ -28,5 +26,17 @@ public class MemberController {
 	public ResponseEntity<?> login(@RequestBody RefreshTokenRequestDto requestDto) throws Exception {
 		JwtTokenDto login = memberService.refreshToken(requestDto.getRefreshToken());
 		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("리프레시 토큰 발급 완료", login));
+	}
+
+	@PostMapping("/join")
+	public ResponseEntity<?> join(@RequestBody MemberRegisterRequestDto requestDto) {
+		memberService.register(requestDto);
+		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("회원 정보 로드 성공", null));
+	}
+
+	@GetMapping("/type")
+	public ResponseEntity<?> memberType(@AuthenticationPrincipal Member member) {
+		MemberFindResponseDto result = memberService.memberType(member);
+		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("회원 여부 조회 성공", result));
 	}
 }
