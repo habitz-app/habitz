d7c17ff26aa5052fb3f0e893da38aa58cc0bd1b2
yhw991228 <yhw991228@gmail.com>
2024-05-02 13:01:07 +0900

    🎨 feat: add quiz history
    Change-Id: I8c6d7eb177c37bb54ddd503ecff4bf841ecf2e15

--- backend/src/main/java/space/habitz/api/domain/quiz/repository/QuizHistoryRepository.java
+++ backend/src/main/java/space/habitz/api/domain/quiz/repository/QuizHistoryRepository.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import space.habitz.api.domain.member.entity.Child;
+import space.habitz.api.domain.quiz.entity.Quiz;
 import space.habitz.api.domain.quiz.entity.QuizHistory;
 
 public interface QuizHistoryRepository extends JpaRepository<QuizHistory, Long> {
 
 	Optional<QuizHistory> findByQuiz_DateAndChildId(LocalDate date, Long childId);
-
+	Optional<QuizHistory> findByChildAndQuiz(Child child, Quiz quiz);
 }
