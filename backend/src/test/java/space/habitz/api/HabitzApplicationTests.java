5fac0857e345e4a961c9bd71910f70391d8f0a8b
itsmo1031 <itsmo1031@gmail.com>
2024-05-02 16:52:21 +0900

    🐛 fix: edit tests file
    Change-Id: Ia6ffdfdb2a3d9cdb1860b192077653975c446fd6

--- backend/src/test/java/space/habitz/api/HabitzApplicationTests.java
+++ backend/src/test/java/space/habitz/api/HabitzApplicationTests.java
@@ -3,7 +3,7 @@ package space.habitz.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HabitzApplicationTests.class)
 class HabitzApplicationTests {
 
 	@Test
