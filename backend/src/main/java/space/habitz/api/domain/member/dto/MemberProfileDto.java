51be983e01f17b7350253402c0fa41a4e03c7746
mblaqsj1015 <mblaqsj1015@naver.com>
2024-05-02 16:45:16 +0900

    ✨ feat: get mission list by parent and date
    - 부모 입장에서 전체 아이의 미션 리스트 조회
    
    Change-Id: Ic8d3362801f0a1b53fc1c9797b4b3129be7aff7a

--- backend/src/main/java/space/habitz/api/domain/member/dto/MemberProfileDto.java
+++ backend/src/main/java/space/habitz/api/domain/member/dto/MemberProfileDto.java
@@ -1,10 +1,17 @@
 package space.habitz.api.domain.member.dto;
 
 import lombok.Builder;
+import space.habitz.api.domain.member.entity.Member;
 
 @Builder
 public record MemberProfileDto(
 	String name,
 	String memberUUID
 ) {
+	public static MemberProfileDto of(Member member) {
+		return MemberProfileDto.builder()
+			.name(member.getName())
+			.memberUUID(member.getUuid())
+			.build();
+	}
 }
