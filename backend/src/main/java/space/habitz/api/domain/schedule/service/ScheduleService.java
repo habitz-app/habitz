d30b80ed3b5ff8d0be7188f4ab04325567366639
mblaqsj1015 <tpwjd29@gmail.com>
2024-05-03 13:58:27 +0900

    ✨ feat: add get scheduleList for date
    - 날짜에 따라 계획된 일정 조회 로직 추가
    - 부모 입장 & 아이 입장 조회 건 구현
    
    Change-Id: I30ccbb9e57b12399cb7d95827df2c2fd0f3e72de

--- backend/src/main/java/space/habitz/api/domain/schedule/service/ScheduleService.java
+++ backend/src/main/java/space/habitz/api/domain/schedule/service/ScheduleService.java
@@ -42,7 +42,7 @@ public class ScheduleService {
 		Member child = memberRepository.findByUuid(scheduleRequestDto.childUUID())
 			.orElseThrow(() -> new CustomErrorException(ErrorCode.CHILD_NOT_FOUND));
 
-		validateFamily(member, child);    // 가족 관계 확인
+		validateFamily(member.getFamily().getId(), child.getFamily().getId());    // 가족 관계 확인
 
 		Schedule schedule = scheduleRequestDto.toEntity(member, child);
 		scheduleRepository.save(schedule);
@@ -61,9 +61,74 @@ public class ScheduleService {
 		Schedule schedule = scheduleRepository.findById(scheduleId)
 			.orElseThrow(() -> new CustomErrorException(ErrorCode.SCHEDULE_NOT_FOUND));
 
-		validateFamily(member, schedule.getChild());    // 가족 관계 확인
+		validateFamily(member.getFamily().getId(), schedule.getChild().getFamily().getId());    // 가족 관계 확인
 
 		return ScheduleDto.of(schedule);
+	}
+
+	/**
+	 * 일정(미션) 목록 조회
+	 * - 로그인한 사용자의 일정 목록을 조회한다.
+	 * - 일정은 날짜로 부여될 때 "미션"과 동일한 형태로 return 한다.
+	 * - 일정은 status가 존재하지 않는다.
+	 *
+	 * @param member 로그인한 사용자
+	 * @param date  조회할 날짜
+	 * */
+	@Transactional(readOnly = true)
+	public List<ScheduleMissionDto> getScheduleMissionList(Member member, LocalDate date) {
+		List<Schedule> scheduleList = scheduleCustomRepository.findByDateWithChildId(date, member.getId());
+
+		return scheduleList.stream()
+			.map(ScheduleMissionDto::of)
+			.toList();
+	}
+
+	/**
+	 * 부모가 자식들의 미션 목록을 조회
+	 * - 로그인 한 유저의 아이 목록을 조회한다.
+	 * - 날짜를 기준으로 아이들의 미션 목록을 조회한다.
+	 * - TODO :: 아이 기준으로 조회 -> 생년월일 순으로 조회되도록 QueryDSL 사용해야함
+	 *
+	 * @param member 로그인한 사용자
+	 * @param date   조회할 날짜
+	 * */
+	@Transactional(readOnly = true)
+	public List<Map<String, Object>> getChildrenScheduleMissionList(Member member, LocalDate date) {
+
+		// 가족 조회
+		Family family = member.getFamily();
+		List<Member> children = memberRepository.findByFamilyIdAndRole(family.getId(), Role.CHILD);
+
+		// 가족의 자식 목록 조회
+		List<Map<String, Object>> totalScheduleMissionList = new ArrayList<>();
+		for (Member child : children) {
+			MemberProfileDto childInfo = MemberProfileDto.of(child);
+			// 자식들의 일정(미션) 목록 조회
+			List<ScheduleMissionDto> scheduleMissionDtoList = getScheduleMissionList(child, date);
+			totalScheduleMissionList.add(Map.of("childInfo", childInfo, "schedules", scheduleMissionDtoList));
+		}
+		return totalScheduleMissionList;
+	}
+
+	/**
+	 * 부모 유저가 특정 자식의 미션 목록을 조회
+	 * - 부모 유저와 child의 가족 관계가 성립해야 한다.
+	 *
+	 * @param member    로그인한 사용자
+	 * @param childUUID 조회할 자식의 UUID
+	 * @param date      조회할 날짜
+	 * */
+	@Transactional(readOnly = true)
+	public List<ScheduleMissionDto> getChildScheduleMissionList(Member member, LocalDate date, String childUUID) {
+		// 자식의 정보 조회
+		Member child = memberRepository.findByUuid(childUUID)
+			.orElseThrow(() -> new CustomErrorException(ErrorCode.CHILD_NOT_FOUND));
+
+		validateFamily(member.getFamily().getId(), child.getFamily().getId());
+
+		// 자식의 scheduleMission 조회
+		return getScheduleMissionList(child, date);
 	}
 
 	/**
@@ -83,14 +148,13 @@ public class ScheduleService {
 	/**
 	 * 같은 가족인지 확인하는 validation
 	 *
-	 * @param parent 가족 관계를 확인할 사용자
-	 * @param child  가족 관계를 확인할 사용자
+	 * @param memberFamilyId 가족 관계를 확인할 사용자
+	 * @param childFamilyId  가족 관계를 확인할 사용자
 	 */
-	public boolean validateFamily(Member parent, Member child) {
-		if (!parent.getFamily().getId().equals(child.getFamily().getId())) {
+	public void validateFamily(String memberFamilyId, String childFamilyId) {
+		if (!memberFamilyId.equals(childFamilyId)) {
 			throw new CustomErrorException(ErrorCode.FAMILY_NOT_MATCH);
 		}
-		return true;
 	}
 
 }
