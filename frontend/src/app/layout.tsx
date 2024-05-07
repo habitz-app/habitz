e8907f302ad70b68c2bce63941d93e2537d94f18
itsmo1031 <itsmo1031@gmail.com>
2024-05-07 10:55:39 +0900

    ✨ feat: add kakao sdk & inject provider
    Change-Id: I81393894a7ac78d1d076888e2396316ed829c1e6

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,11 +2,19 @@ import type { Metadata } from 'next';
 import { Pretendard } from './fonts';
 import './globals.css';
 import { css } from 'styled-system/css';
+import Script from 'next/script';
+import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'habitz',
   description: 'Earn habitz from your parents by completing mission!',
 };
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={Pretendard.variable}>
-      <body className={css({ font: 'pretendard' })}>{children}</body>
+      <body className={css({ font: 'pretendard' })}>
+        <Providers>
+          {children}
+          <Script
+            src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.1/kakao.min.js"
+            integrity="sha384-kDljxUXHaJ9xAb2AzRd59KxjrFjzHa5TAoFQ6GbYTCAG0bjM55XohjjDT7tDDC01"
+            crossOrigin="anonymous"
+            strategy="beforeInteractive"
+          />
+        </Providers>
+      </body>
     </html>
   );
 }
