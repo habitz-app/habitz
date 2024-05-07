fa7c4bd6234ba68d98545d67a4391de55994eac1
용희원 <yhw991228@gmail.com>
2024-05-07 10:36:48 +0900

    ✨ feat: add point history for child , family not yet
    Change-Id: Ibcbacee07de81740e59c59fe452ae5769578216c

--- backend/src/main/java/space/habitz/api/domain/point/entity/FamilyPointHistory.java
+++ backend/src/main/java/space/habitz/api/domain/point/entity/FamilyPointHistory.java
@@ -9,12 +9,16 @@ public class FamilyPointHistory {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(name = "remain_point")
 	private int remainPoint;
+
 	@Column(name = "pay_point")
 	private int payPoint;
+
 	@Column(name = "charge_id") // 부모 포인트 결제 내역 id
 	private Long chargeId;
+
 	@Column(name = "payment_id") // 자녀 미션 승인 id
 	private Long paymentId;
 
