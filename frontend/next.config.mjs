b25b431a39465eaa12366afb70d5571f3d09c2d9
정필모 <itsmo1031@gmail.com>
2024-05-07 10:55:39 +0900

    🔧 chore: add proxy
    Change-Id: I989ae05f1d85dce1b543af43a8593fc7fc677276

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
     // 테스트용 bing 이미지
     remotePatterns: [{ protocol: 'https', hostname: '*.bing.com' }],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.SERVER_URL}/:path*`,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
