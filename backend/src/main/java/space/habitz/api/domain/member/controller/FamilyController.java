336fcbbe82dcc7be02e62225c97794efecb56896
조승우 <kjsw12@naver.com>
2024-05-08 15:23:17 +0900

    🔥 fix: 컨트롤러 수정
    Change-Id: I854b9f6162f8a7de89f049d0f36efa489c724756

--- backend/src/main/java/space/habitz/api/domain/member/controller/FamilyController.java
+++ backend/src/main/java/space/habitz/api/domain/member/controller/FamilyController.java
@@ -1,15 +1,18 @@
 package space.habitz.api.domain.member.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import space.habitz.api.domain.member.dto.*;
+import space.habitz.api.domain.member.dto.FamilyListResponseDto;
+import space.habitz.api.domain.member.dto.MemberInviteCodeResponse;
 import space.habitz.api.domain.member.service.FamilyService;
 import space.habitz.api.global.response.ApiResponseData;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/family")
@@ -18,21 +21,21 @@ public class FamilyController {
 	private final FamilyService familyService;
 
 	@GetMapping("/inviteCode")
-	public ResponseEntity<?> inviteCode(){
+	public ResponseEntity<?> inviteCode() {
 		MemberInviteCodeResponse inviteCode = familyService.getInviteCode();
 		return ApiResponseData.success(inviteCode);
 	}
 
 	@PreAuthorize("hasAnyRole('PARENT')")
 	@GetMapping("/memberList")
-	public ResponseEntity<?> memberList(){
+	public ResponseEntity<?> memberList() {
 		List<FamilyListResponseDto> familyList = familyService.getFamilyList();
 		return ApiResponseData.success(familyList);
 	}
 
 	@PreAuthorize("hasAnyRole('PARENT')")
 	@GetMapping("/childList")
-	public ResponseEntity<?> childList(){
+	public ResponseEntity<?> childList() {
 		List<FamilyListResponseDto> familyList = familyService.getChildList();
 		return ApiResponseData.success(familyList);
 	}
