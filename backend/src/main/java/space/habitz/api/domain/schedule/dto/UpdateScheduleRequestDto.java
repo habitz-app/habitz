ec4562926a5d716f5f3d2153c24cbeeb1bc0bd26
mblaqsj1015 <mblaqsj1015@naver.com>
2024-05-08 10:24:36 +0900

    ✨ feat: update function, create an update method within the entity and add UpdateScheduleRequestDto
    - 스케줄 엔티티에서 업데이트 로직을 추가합니다.
    - UpdateScheduleRequestDto 구현
    
    Change-Id: I20e4f0d1477b662f57b7894d6f83974dced426f8

---
+++ backend/src/main/java/space/habitz/api/domain/schedule/dto/UpdateScheduleRequestDto.java
@@ -0,0 +1,17 @@
+package space.habitz.api.domain.schedule.dto;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+
+@Builder
+public record UpdateScheduleRequestDto(
+	String title,
+	String content,
+	String emoji,
+	LocalDate startDate,
+	LocalDate endDate,
+	Boolean[] weekDays,
+	Integer point
+) {
+}
