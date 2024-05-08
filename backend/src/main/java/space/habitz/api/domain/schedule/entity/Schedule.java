466006b924ac25aaf3cb09f96c86750bf78a3320
mblaqsj1015 <tpwjd29@gmail.com>
2024-05-08 10:24:36 +0900

    ✨ feat: update function, create an update method within the entity and add UpdateScheduleRequestDto
    - 스케줄 엔티티에서 업데이트 로직을 추가합니다.
    - UpdateScheduleRequestDto 구현
    
    Change-Id: I20e4f0d1477b662f57b7894d6f83974dced426f8

--- backend/src/main/java/space/habitz/api/domain/schedule/entity/Schedule.java
+++ backend/src/main/java/space/habitz/api/domain/schedule/entity/Schedule.java
@@ -1,15 +1,25 @@
 package space.habitz.api.domain.schedule.entity;
 
-import jakarta.persistence.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.mission.entity.Mission;
+import space.habitz.api.domain.schedule.dto.UpdateScheduleRequestDto;
 import space.habitz.api.global.entity.MutableTimeEntity;
-
-import java.time.LocalDate;
-import java.util.List;
-
 
 @Getter
 @Setter
@@ -68,4 +78,37 @@ public class Schedule extends MutableTimeEntity {
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "schedule")
 	private List<Mission> missionList;
+
+	public void updateSchedule(UpdateScheduleRequestDto request) {
+
+		// Convert List to Column
+		LocalDate updateStartDate = request.startDate();
+		LocalDate updateEndDate = request.endDate();
+		Boolean[] updateWeekDays = request.weekDays();
+
+		boolean isRepeatable = !updateStartDate.equals(updateEndDate); // 반복 여부 확인
+
+		if (!isRepeatable) {
+			DayOfWeek dayOfWeek = updateStartDate.getDayOfWeek();
+			Arrays.fill(updateWeekDays, Boolean.FALSE);
+			updateWeekDays[dayOfWeek.getValue() - 1] = Boolean.TRUE;
+		}
+
+		// update
+		this.title = request.title();
+		this.content = request.content();
+		this.emoji = request.emoji();
+		this.point = request.point();
+		this.startDate = updateStartDate;
+		this.endDate = updateEndDate;
+		this.monday = updateWeekDays[0];
+		this.tuesday = updateWeekDays[1];
+		this.wednesday = updateWeekDays[2];
+		this.thursday = updateWeekDays[3];
+		this.friday = updateWeekDays[4];
+		this.saturday = updateWeekDays[5];
+		this.sunday = updateWeekDays[6];
+		this.repeatable = isRepeatable;
+
+	}
 }
