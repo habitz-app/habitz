54f628303df025a990e926ac0c94ed1723282edb
yhw991228 <yhw991228@gmail.com>
2024-05-08 16:52:50 +0900

    ✨ feat: add article
    Change-Id: I602058b049ec548ddd46dab327475cccf4adadea

--- backend/src/main/java/space/habitz/api/domain/quiz/dto/QuizInfoDto.java
+++ backend/src/main/java/space/habitz/api/domain/quiz/dto/QuizInfoDto.java
@@ -13,8 +13,6 @@ import lombok.NoArgsConstructor;
 @Getter
 public class QuizInfoDto {
 	Long id;
-
 	String content;
-
 	LocalDate createdAt;
 }
--- backend/src/main/java/space/habitz/api/domain/quiz/entity/Quiz.java
+++ backend/src/main/java/space/habitz/api/domain/quiz/entity/Quiz.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
+import space.habitz.api.domain.article.entity.Article;
 
 @Entity
 @Getter
@@ -25,6 +29,9 @@ public class Quiz {
 	private String commentary;
 	@Column(name = "recommended_age")
 	private int recommendedAge;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "article_id")
+	Article article;
 	@Column(name = "date")
 	private LocalDate date;
 
