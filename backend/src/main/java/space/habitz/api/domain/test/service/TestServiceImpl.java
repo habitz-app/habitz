c763dcbc0f37c38ff56ca4a719f2936086c4cd7b
yhw991228 <yhw991228@gmail.com>
2024-05-08 10:56:29 +0900

    🚑️ fix: fix type int
    Change-Id: I34a5bbae26e9a8b712190d8f7a7fec6d41e669f1

--- backend/src/main/java/space/habitz/api/domain/test/service/TestServiceImpl.java
+++ backend/src/main/java/space/habitz/api/domain/test/service/TestServiceImpl.java
@@ -1,12 +1,33 @@
 package space.habitz.api.domain.test.service;
 
-import lombok.RequiredArgsConstructor;
+import static space.habitz.api.global.util.RandomUtils.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 import space.habitz.api.domain.member.dto.JwtTokenDto;
 import space.habitz.api.domain.member.dto.MemberLoginResultDto;
-import space.habitz.api.domain.member.entity.*;
+import space.habitz.api.domain.member.entity.Child;
+import space.habitz.api.domain.member.entity.Family;
+import space.habitz.api.domain.member.entity.Member;
+import space.habitz.api.domain.member.entity.MemberProfile;
+import space.habitz.api.domain.member.entity.Parent;
+import space.habitz.api.domain.member.entity.RefreshToken;
+import space.habitz.api.domain.member.entity.Role;
+import space.habitz.api.domain.member.entity.SocialInform;
 import space.habitz.api.domain.member.exeption.MemberNotFoundException;
-import space.habitz.api.domain.member.repository.*;
+import space.habitz.api.domain.member.repository.ChildRepository;
+import space.habitz.api.domain.member.repository.FamilyRepository;
+import space.habitz.api.domain.member.repository.MemberProfileRepository;
+import space.habitz.api.domain.member.repository.MemberRepository;
+import space.habitz.api.domain.member.repository.ParentRepository;
+import space.habitz.api.domain.member.repository.RefreshTokenRepository;
+import space.habitz.api.domain.member.repository.SocialInformRepository;
 import space.habitz.api.domain.member.service.JwtTokenProvider;
 import space.habitz.api.domain.test.dto.DummyFamilyMakeRequestDto;
 import space.habitz.api.domain.test.dto.DummyMemberLoginRequestDto;
@@ -14,15 +35,6 @@ import space.habitz.api.domain.test.dto.DummyMemberRegisterRequestDto;
 import space.habitz.api.domain.test.dto.DummyMemberRegisterResponseDto;
 import space.habitz.api.global.exception.CustomErrorException;
 import space.habitz.api.global.util.RandomUtils;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static space.habitz.api.global.util.RandomUtils.generateRandomCode;
-
 
 @Service
 @RequiredArgsConstructor
@@ -74,23 +86,23 @@ public class TestServiceImpl implements TestService {
 			.orElseGet(
 				() -> {
 					String randomCode = RandomUtils.generateRandomCode(6);
-					Family generatedFamily = new Family(randomCode, 0L);
+					Family generatedFamily = new Family(randomCode, 0);
 					return familyRepository.saveAndFlush(generatedFamily);
 				}
 			);
 
 		String memberRole = requestDto.getRole().toUpperCase();
 		Role role = Role.findEnum(memberRole);
-		
+
 		member.setRole(role);
 		member.setFamily(family);
 		member = memberRepository.save(member);
 
 		if (role.getRoleName().equals("PARENT")) {
-			Parent parent = new Parent(member, 0L);
+			Parent parent = new Parent(member, 0);
 			parentRepository.save(parent);
 		} else if (role.getRoleName().equals("CHILD")) {
-			Child child = new Child(member, 0L);
+			Child child = new Child(member, 0);
 			childRepository.save(child);
 		}
 
@@ -138,28 +150,32 @@ public class TestServiceImpl implements TestService {
 
 	@Override
 	public List<MemberLoginResultDto> makeFamily(DummyFamilyMakeRequestDto requestDto) {
-		Member member = memberRepository.findByUserId(requestDto.getMemberId()).orElseThrow(() -> new MemberNotFoundException(requestDto.getMemberId()));
+		Member member = memberRepository.findByUserId(requestDto.getMemberId())
+			.orElseThrow(() -> new MemberNotFoundException(requestDto.getMemberId()));
 
-		if(!member.getRole().equals(Role.PARENT)) throw new CustomErrorException("부모 코스프레는 허용하지 않습니다.");
+		if (!member.getRole().equals(Role.PARENT))
+			throw new CustomErrorException("부모 코스프레는 허용하지 않습니다.");
 
-        return IntStream.range(0, Math.toIntExact(requestDto.getNumber()))
-            .mapToObj(i -> {
-                DummyMemberRegisterRequestDto dto = DummyMemberRegisterRequestDto.builder()
-                    .familyId(member.getFamily().getId())
-                    .nickName("나의 배포물" + i)
-                    .birthDate(LocalDate.now())
-                    .role(Role.CHILD.getRoleName().toUpperCase())
-                    .name("자식" + i)
-                    .image("")
-                    .gender(new Random().nextInt(2) > 0 ? "MALE" : "FEMALE")
-                    .email("deploy" + i + "@kakao.com")
-                    .build();
+		return IntStream.range(0, Math.toIntExact(requestDto.getNumber()))
+			.mapToObj(i -> {
+				DummyMemberRegisterRequestDto dto = DummyMemberRegisterRequestDto.builder()
+					.familyId(member.getFamily().getId())
+					.nickName("나의 배포물" + i)
+					.birthDate(LocalDate.now())
+					.role(Role.CHILD.getRoleName().toUpperCase())
+					.name("자식" + i)
+					.image("")
+					.gender(new Random().nextInt(2) > 0 ? "MALE" : "FEMALE")
+					.email("deploy" + i + "@kakao.com")
+					.build();
 				return this.register(dto);
-            }).map(
-                dummyMemberRegisterResponseDto -> {
-                    DummyMemberLoginRequestDto dummyMemberLoginRequestDto = new DummyMemberLoginRequestDto(dummyMemberRegisterResponseDto.getUserId());
-                    return this.getAccessToken(dummyMemberLoginRequestDto);
-                }
-            ).toList();
+			}).map(
+				dummyMemberRegisterResponseDto -> {
+					DummyMemberLoginRequestDto dummyMemberLoginRequestDto = new DummyMemberLoginRequestDto(
+						dummyMemberRegisterResponseDto.getUserId());
+					return this.getAccessToken(dummyMemberLoginRequestDto);
+				}
+			).toList();
 	}
 }
+
