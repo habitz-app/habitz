cba511b9b88d51f29ed0055ca2dbe0874f1b5af7
itsmo1031 <itsmo1031@gmail.com>
2024-05-09 15:47:58 +0900

    💄 feat: add KakaoLoginButton
    Change-Id: I48ab24637059321f57209c8064fb50d47a223ff3

---
+++ frontend/src/components/login/KakaoLoginButton.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import KakaoLoginButton from './KakaoLoginButton';
+
+export default {
+  title: 'KakaoLoginButton',
+  component: KakaoLoginButton,
+} as Meta;
+
+type Story = StoryObj<typeof KakaoLoginButton>;
+
+export const Default: Story = {};
