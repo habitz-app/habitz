07a26901beec87b3367c21805d6ca8a913f88660
yhw991228 <yhw991228@gmail.com>
2024-05-09 16:26:30 +0900

    ✨ feat: add history service family & child
    Change-Id: I63f590ac41546b44082573ec2ae128ca8ad2a5de

--- backend/src/main/java/space/habitz/api/domain/point/service/FamilyPointHistoryService.java
+++ backend/src/main/java/space/habitz/api/domain/point/service/FamilyPointHistoryService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.point.dto.PointHistory;
 
-public interface ChildPointHistoryService {
-
+public interface FamilyPointHistoryService {
 	List<PointHistory> getPointHistory(Member member, LocalDate startDate, LocalDate endDate);
 }
