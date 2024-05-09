b4a986989ba7074039814f7f231f3333ad9b8f5c
yhw991228 <yhw991228@gmail.com>
2024-05-09 16:24:38 +0900

    ✨ fix: fix the repository add date
    Change-Id: I39fcda00c77b57df25223ed8a11670cc081f1fa1

--- backend/src/main/java/space/habitz/api/domain/point/repository/FamilyPointHistoryRepository.java
+++ backend/src/main/java/space/habitz/api/domain/point/repository/FamilyPointHistoryRepository.java
@@ -1,9 +1,13 @@
 package space.habitz.api.domain.point.repository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import space.habitz.api.domain.point.entity.FamilyPointHistory;
 
 public interface FamilyPointHistoryRepository extends JpaRepository<FamilyPointHistory, Long> {
-	
+	List<FamilyPointHistory> findFamilyPointHistoriesByFamily_IdAndCreatedAtBetweenOrderByCreatedAtDesc(
+		String familyId, Timestamp createdAt, Timestamp createdAt2);
 }
