593ab52124e0acff34f31d6c1a8c5b91757184e2
yhw991228 <yhw991228@gmail.com>
2024-05-09 16:28:10 +0900

    ✨ feat: add controller function for search family history
    Change-Id: I7cc9b30626ffb4d5abcf32d42c754bf754466a1f

--- backend/src/main/java/space/habitz/api/domain/point/controller/PointHistoryController.java
+++ backend/src/main/java/space/habitz/api/domain/point/controller/PointHistoryController.java
@@ -1,11 +1,13 @@
 package space.habitz.api.domain.point.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -13,25 +15,30 @@ import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.member.entity.Role;
 import space.habitz.api.domain.point.dto.PointHistory;
 import space.habitz.api.domain.point.service.ChildPointHistoryService;
+import space.habitz.api.domain.point.service.FamilyPointHistoryService;
 import space.habitz.api.global.exception.CustomNotFoundException;
 import space.habitz.api.global.response.ResponseData;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/point")
-@PreAuthorize("hasAnyRole('PARENT', 'ADMIN')")
 public class PointHistoryController {
 
 	private final ChildPointHistoryService childPointHistoryService;
+	private final FamilyPointHistoryService familyPointHistoryService;
 
 	@GetMapping("/history")
-	public ResponseData<List<PointHistory>> getPointHistory(@AuthenticationPrincipal Member member) {
+	public ResponseData<List<PointHistory>> getPointHistory(@AuthenticationPrincipal Member member,
+		@RequestParam("start") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+		@RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
+	) {
 		if (member.getRole() == Role.PARENT) {
-			// family 포인트 내역 조회 예정, 현재는 자녀 포인트 내역만 조회 가능
-			return null;
+			return new ResponseData<>("success", "포인트 내역 조회 성공",
+				familyPointHistoryService.getPointHistory(member, startDate, endDate.plusDays(1)));
 		}
 		if (member.getRole() == Role.CHILD) {
-			return new ResponseData<>("success", "포인트 내역 조회 성공", childPointHistoryService.getPointHistory(member));
+			return new ResponseData<>("success", "포인트 내역 조회 성공",
+				childPointHistoryService.getPointHistory(member, startDate, endDate.plusDays(1)));
 		}
 
 		throw new CustomNotFoundException("부모, 자녀 계정이 아닙니다.");
