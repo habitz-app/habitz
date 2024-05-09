df12a2b120deaade53680dea51f92229f14b250e
조승우 <kjsw12@naver.com>
2024-05-09 09:54:19 +0900

    🔥 fix: 테스트 컨트롤러 로그인시 토큰 발급시 쿠키로 발급
    Change-Id: I0f0b39764ea7be7d909085d7243f4bdacc6f446c

--- backend/src/main/java/space/habitz/api/domain/test/controller/TestController.java
+++ backend/src/main/java/space/habitz/api/domain/test/controller/TestController.java
@@ -1,9 +1,19 @@
 package space.habitz.api.domain.test.controller;
 
-import lombok.RequiredArgsConstructor;
+import java.time.Duration;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 import space.habitz.api.domain.member.dto.MemberLoginResultDto;
 import space.habitz.api.domain.test.dto.DummyFamilyMakeRequestDto;
 import space.habitz.api.domain.test.dto.DummyMemberLoginRequestDto;
@@ -11,9 +21,6 @@ import space.habitz.api.domain.test.dto.DummyMemberRegisterRequestDto;
 import space.habitz.api.domain.test.dto.DummyMemberRegisterResponseDto;
 import space.habitz.api.domain.test.service.TestService;
 import space.habitz.api.global.response.ResponseData;
-
-import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/v1/test")
@@ -27,22 +34,43 @@ public class TestController {
 		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("회원 가입 성공", register));
 	}
 
-	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody DummyMemberLoginRequestDto request) throws Exception {
-		MemberLoginResultDto accessToken = testService.getAccessToken(request);
-		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("로그인 성공", accessToken));
+	private static ResponseCookie getCookie(String key, String value, int days) {
+		return ResponseCookie.from(key, value)
+			.httpOnly(true)
+			.secure(true)
+			.sameSite("None")
+			.path("/")
+			.maxAge(Duration.ofDays(days))
+			.build();
 	}
 
-	@PostMapping("/makeFamily/{memberId}/{targetId}")
-	public ResponseEntity<?> makeFamily(@PathVariable("memberId") Long memberId, @PathVariable("targetId") Long targetId) {
-		testService.makeFamily(memberId, targetId);
-		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("가족 등록 성공", null));
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody DummyMemberLoginRequestDto request) throws Exception {
+		MemberLoginResultDto result = testService.getAccessToken(request);
+		ResponseCookie accessToken = getCookie("accessToken", result.getJwtTokenDto().getAccessToken(), 1);
+		ResponseCookie refreshToken = getCookie("refreshToken", result.getJwtTokenDto().getRefreshToken(), 7);
+		ResponseCookie tokenType = getCookie("tokenType", result.getJwtTokenDto().getTokenType(), 1);
+		ResponseCookie role = getCookie("role", result.getRole().toString(), 1);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.header(HttpHeaders.SET_COOKIE, refreshToken.toString())
+			.header(HttpHeaders.SET_COOKIE, accessToken.toString())
+			.header(HttpHeaders.SET_COOKIE, tokenType.toString())
+			.header(HttpHeaders.SET_COOKIE, role.toString())
+			.body(ResponseData.success("로그인 성공", result));
 	}
 
 	@PostMapping("/makeFamily/")
 	public ResponseEntity<?> makeFamily(@RequestBody DummyFamilyMakeRequestDto familyMakeRequestDto) {
 		List<MemberLoginResultDto> result = testService.makeFamily(familyMakeRequestDto);
 		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("가족 등록 성공", result));
+	}
+
+	@PostMapping("/makeFamily/{memberId}/{targetId}")
+	public ResponseEntity<?> makeFamily(@PathVariable("memberId") Long memberId,
+		@PathVariable("targetId") Long targetId) {
+		testService.makeFamily(memberId, targetId);
+		return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("가족 등록 성공", null));
 	}
 
 }
