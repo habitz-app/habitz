462f4a94617464fca0743c3f9ed6ce359ef0f9bb
mblaqsj1015 <tpwjd29@gmail.com>
2024-05-09 09:30:20 +0900

    🐛 fix: fix 0e5c0a9 commit error
    - 0e5c0a9 Commit에서 fileupload 폴더 경로를 이동하면서 누락한 파일 추가
    - UploadFileResponseDto 컨벤션 미적용 줄바꿈 수정
    
    Change-Id: Ia0a8aa5fd22d123171b40be84655ee25deb99dc2

--- backend/src/main/java/space/habitz/api/global/util/fileupload/dto/UploadedFileResponseDto.java
+++ backend/src/main/java/space/habitz/api/global/util/fileupload/dto/UploadedFileResponseDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UploadedFileResponseDto {
-    private String originalFile;
-    private String saveFile;
+	private String originalFile;
+	private String saveFile;
 }
