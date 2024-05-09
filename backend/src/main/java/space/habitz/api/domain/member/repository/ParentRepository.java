5b59b604912c71879139716fbdd0f03a7723b722
조승우 <kjsw12@naver.com>
2024-05-09 11:15:26 +0900

    🔥 fix: 부모, 자식 Repository 쿼리 추가
    Change-Id: I441fd6ffd10843b6d6432ff530ea56ed4d7ac2ea

--- backend/src/main/java/space/habitz/api/domain/member/repository/ParentRepository.java
+++ backend/src/main/java/space/habitz/api/domain/member/repository/ParentRepository.java
@@ -1,9 +1,14 @@
 package space.habitz.api.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.member.entity.Parent;
 
 @Repository
 public interface ParentRepository extends JpaRepository<Parent, Long> {
+	Optional<Parent> findByMember(Member member);
 }
