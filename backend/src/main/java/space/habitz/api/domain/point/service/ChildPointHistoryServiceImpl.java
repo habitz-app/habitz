fe5bd34f7a37783886a9af7aa4d437f16fdd5c63
yhw991228 <yhw991228@gmail.com>
2024-05-09 16:26:30 +0900

    ✨ feat: add history service family & child
    Change-Id: I63f590ac41546b44082573ec2ae128ca8ad2a5de

--- backend/src/main/java/space/habitz/api/domain/point/service/ChildPointHistoryServiceImpl.java
+++ backend/src/main/java/space/habitz/api/domain/point/service/ChildPointHistoryServiceImpl.java
@@ -1,5 +1,7 @@
 package space.habitz.api.domain.point.service;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,27 +13,28 @@ import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.member.entity.Role;
 import space.habitz.api.domain.member.repository.ChildRepository;
 import space.habitz.api.domain.point.dto.PointHistory;
-import space.habitz.api.domain.point.entity.ChildPointHistory;
 import space.habitz.api.domain.point.repository.ChildPointHistoryRepository;
 
 @Service
 @RequiredArgsConstructor
-public class ChildPointHistoryServiceImpl implements ChildPointHistoryService{
+public class ChildPointHistoryServiceImpl implements ChildPointHistoryService {
 
 	private final ChildPointHistoryRepository childPointHistoryRepository;
 	private final ChildRepository childRepository;
 
 	@Override
-	public List<PointHistory> getPointHistory(Member member) {
-		if(member.getRole() != Role.CHILD) {
-			throw new IllegalArgumentException("member is not child") ;
+	public List<PointHistory> getPointHistory(Member member, LocalDate startDate, LocalDate endDate) {
+		if (member.getRole() != Role.CHILD) {
+			throw new IllegalArgumentException("member is not child");
 		}
 		Child child = childRepository.findByMember_Id(member.getId());
 
-		return childPointHistoryRepository.findChildPointHistoriesByChild_IdOrderByCreatedAtDesc(child.getId())
+		return childPointHistoryRepository.findChildPointHistoriesByChild_IdAndCreatedAtBetweenOrderByCreatedAtDesc(
+				child.getId(), Timestamp.valueOf(startDate.atStartOfDay()), Timestamp.valueOf(endDate.atStartOfDay()))
 			.stream()
 			.map(childPoint -> PointHistory.builder()
 				.point(childPoint.getPoint())
+				.point(childPoint.getTotalPoint())
 				.content(childPoint.getContent())
 				.date(childPoint.getCreatedAt())
 				.build())
