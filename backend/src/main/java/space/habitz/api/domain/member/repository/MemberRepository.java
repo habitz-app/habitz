4df906406977a7268025228cc02c964c3fe87308
mblaqsj1015 <mblaqsj1015@naver.com>
2024-05-10 10:01:19 +0900

    ♻️ refactor: delete no usage on repository layer
    - 사용되지 않는 리포지토리 레이어 메서드 삭제
    
    Change-Id: I2cf1264bf924e32d34d49886f556a17365eb664e

--- backend/src/main/java/space/habitz/api/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/space/habitz/api/domain/member/repository/MemberRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import space.habitz.api.domain.member.entity.Member;
-import space.habitz.api.domain.member.entity.Role;
-import space.habitz.api.domain.member.entity.SocialInform;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
-	Optional<Member> findBySocialInform(SocialInform socialInform);
 
 	Optional<Member> findByUuid(String uuid);
 }
