5407f61db6f29f0a6e24f641b42315535b296275
kimgiraffe <kimgiraffe@naver.com>
2024-05-10 14:55:47 +0900

    ✨ feat: add child store page header
    Change-Id: I49f63ef025183af2bbb61b6487877f16f6822611

--- frontend/src/app/(child)/store/page.tsx
+++ frontend/src/app/(child)/store/page.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import StoreCategory from '@/components/store/StoreCategory';
+import Image from 'next/image';
 import Link from 'next/link';
+import { css } from 'styled-system/css';
 import { Stack } from 'styled-system/jsx';
 
 const Category = () => {
@@ -14,22 +18,76 @@ const Category = () => {
     { type: 'iceCream', name: '음료/아이스크림' },
   ];
   return (
-    <Stack h={'100%'} justify={'space-between'} px="1rem">
-      {categories.map((category, id) => (
+    <>
+      <header
+        className={css({
+          display: 'flex',
+          position: 'sticky',
+          height: '2.5rem',
+          top: 0,
+          bg: 'background.normal.normal/80',
+          backdropFilter: 'auto',
+          backdropBlur: 'sm',
+          px: '1rem',
+          justifyContent: 'space-between',
+          alignItems: 'end',
+        })}
+      >
         <Link
-          key={id}
-          href={{
-            pathname: '/store/category',
-            query: { category: category.name },
-          }}
+          className={css({
+            fontFamily: 'yeoljeong',
+            fontSize: '28px',
+            lineHeight: '38.02px',
+            color: 'label.alternative',
+          })}
+          href={'/home'}
         >
-          <StoreCategory
-            type={category.type}
-            name={category.name}
-          ></StoreCategory>
+          habitz
         </Link>
-      ))}
-    </Stack>
+        <span
+          className={css({
+            display: 'flex',
+            textStyle: 'headline1.bold',
+            gap: '0.125rem',
+            py: '0.25rem',
+          })}
+        >
+          8430 <Image src="/coin.svg" alt="coin" width={18} height={18} />
+        </span>
+      </header>
+      <div
+        className={css({
+          display: 'flex',
+          w: 'full',
+          flexDir: 'column',
+          gap: '1rem',
+          p: '1rem',
+        })}
+      >
+        <Stack
+          h={'100%'}
+          justify={'space-between'}
+          display="flex"
+          flexDir="column"
+          gap="1.25rem"
+        >
+          {categories.map((category, id) => (
+            <Link
+              key={id}
+              href={{
+                pathname: '/store/category',
+                query: { category: category.name },
+              }}
+            >
+              <StoreCategory
+                type={category.type}
+                name={category.name}
+              ></StoreCategory>
+            </Link>
+          ))}
+        </Stack>
+      </div>
+    </>
   );
 };
 export default Category;
