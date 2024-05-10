c2efc7e4361bc4310ee89d4329b95523c5983b5d
조승우 <kjsw12@naver.com>
2024-05-10 10:56:38 +0900

    🔥 fix: 가족코드 조회 에러
    Change-Id: I80f59d690b2bb519e4cbeda3c7f72384ce542939

--- backend/src/main/java/space/habitz/api/domain/member/dto/MemberInviteCodeResponse.java
+++ backend/src/main/java/space/habitz/api/domain/member/dto/MemberInviteCodeResponse.java
@@ -1,8 +1,12 @@
 package space.habitz.api.domain.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class MemberInviteCodeResponse {
-    private String inviteCode;
+	private String inviteCode;
 }
