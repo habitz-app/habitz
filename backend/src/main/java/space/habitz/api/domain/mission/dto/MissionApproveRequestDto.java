ed152f67d8fb00ca6804a7e5e5fd4c2b2f77a183
mblaqsj1015 <mblaqsj1015@naver.com>
2024-05-13 14:44:35 +0900

    ♻️ refactor: validation of schedule from startdate to enddate
    - validation Size, Emoji 등 어노테이션 적용
    - HackDay 컨벤션 재적용
    
    Change-Id: I9ca7d2f2020f6cd00dfda4781783a114a72123a0

--- backend/src/main/java/space/habitz/api/domain/mission/dto/MissionApproveRequestDto.java
+++ backend/src/main/java/space/habitz/api/domain/mission/dto/MissionApproveRequestDto.java
@@ -1,12 +1,16 @@
 package space.habitz.api.domain.mission.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import space.habitz.api.domain.mission.entity.StatusCode;
 
 @Builder
 public record MissionApproveRequestDto(
 	Long missionId,
+	@NotNull(message = "올바른 상태코드를 입력해주세요.")
 	StatusCode status,
+	@Size(max = 255, message = "인증 내용은 255자를 넘을 수 없습니다.")
 	String comment
 ) {
 
