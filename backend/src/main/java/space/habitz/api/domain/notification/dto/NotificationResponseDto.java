3b1e036ddf7c2c9f86a4c90cfc8dd07fcc61dd1e
조승우 <kjsw12@naver.com>
2024-05-14 16:12:52 +0900

    🔥 fix: 알림 기능 Dto 수정
    Change-Id: Icfe058d7198212332de7dd96eeb9e8bf2b50ca42

--- backend/src/main/java/space/habitz/api/domain/notification/dto/NotificationResponseDto.java
+++ backend/src/main/java/space/habitz/api/domain/notification/dto/NotificationResponseDto.java
@@ -1,6 +1,9 @@
 package space.habitz.api.domain.notification.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import space.habitz.api.domain.notification.entity.Notification;
 import space.habitz.api.domain.notification.entity.NotificationType;
 
@@ -14,14 +17,16 @@ public class NotificationResponseDto {
 	private NotificationType type;
 	private String title;
 	private String content;
+	private String memberUuid;
 	private boolean isRead;
 
-	public static NotificationResponseDto convertTo(Notification notification){
+	public static NotificationResponseDto convertTo(Notification notification) {
 		return NotificationResponseDto.builder()
 			.notificationId(notification.getId())
 			.title(notification.getTitle())
 			.content(notification.getContent())
 			.memberId(notification.getMember().getId())
+			.memberUuid(notification.getMember().getUuid())
 			.type(notification.getType())
 			.isRead(notification.getReadAt() == null)
 			.build();
