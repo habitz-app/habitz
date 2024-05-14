4299fab2951bcb16273bb39f316d7b8646bf791c
itsmo1031 <itsmo1031@gmail.com>
2024-05-14 13:27:20 +0900

    ♻️ refactor: move root page to common section
    Change-Id: I386cc52bb431348168231736471a86e934f8f1d2

--- frontend/src/app/(common)/page.tsx
+++ frontend/src/app/(common)/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
-import { useAuth, useAuthWithRoles } from '@/hooks/useAuth';
 import { css } from 'styled-system/css';
 
 export default function Home() {
-  useAuthWithRoles(['PARENT', 'CHILD']);
-
   return (
     <main>
       <p className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</p>
