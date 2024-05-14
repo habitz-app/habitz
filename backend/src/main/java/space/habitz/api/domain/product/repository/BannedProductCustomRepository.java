6672b42909c31a2a5956b4775687b4bcb0228655
yhw991228 <yhw991228@gmail.com>
2024-05-14 15:15:33 +0900

    ✨ feat: check my children is banned
    Change-Id: Ie8c57deb223b9e5ac2a1bfcc26c9264fcb260199

--- backend/src/main/java/space/habitz/api/domain/product/repository/BannedProductCustomRepository.java
+++ backend/src/main/java/space/habitz/api/domain/product/repository/BannedProductCustomRepository.java
@@ -1,11 +1,16 @@
 package space.habitz.api.domain.product.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import space.habitz.api.domain.member.entity.Member;
+import space.habitz.api.domain.product.dto.ChildBannedProductInfo;
 import space.habitz.api.domain.product.entity.Product;
 
 public interface BannedProductCustomRepository {
 	Page<Product> findProductsByChildId(Long childId, Pageable pageable);
 
+	List<ChildBannedProductInfo> findBannedProductInfo(Member parent, Long productId);
 }
