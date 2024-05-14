72a47673f145b8669d38c5291bc3c3fc6a7487e6
mblaqsj1015 <mblaqsj1015@naver.com>
2024-05-15 00:19:56 +0900

    ♻️ refactor: refactor article information
    - 아티클에 표시하는 정보를 추가합니다.
    
    Change-Id: I1d86ffb51f2c4f7b5f5d6e85236cdb29fc8dc3d7

--- backend/src/main/java/space/habitz/api/domain/article/dto/ArticleInfoDto.java
+++ backend/src/main/java/space/habitz/api/domain/article/dto/ArticleInfoDto.java
@@ -2,6 +2,7 @@ package space.habitz.api.domain.article.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import space.habitz.api.domain.article.entity.Article;
 
 @Builder
 @Getter
@@ -9,4 +10,22 @@ public class ArticleInfoDto {
 	Long id;
 	String title;
 	String content;
+	String previewImage;
+	String writerName;
+	String writerImage;
+	String source;
+	String url;
+
+	public static ArticleInfoDto of(Article article) {
+		return ArticleInfoDto.builder()
+			.id(article.getId())
+			.title(article.getTitle())
+			.content(article.getContent())
+			.previewImage(article.getImageUrl())
+			.writerName(article.getWriterName())
+			.writerImage(article.getWriterImage())
+			.source(article.getSource())
+			.url(article.getUrl())
+			.build();
+	}
 }
