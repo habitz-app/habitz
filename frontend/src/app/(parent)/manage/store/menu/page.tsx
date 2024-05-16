579eca0aa4258446898af2a4504bfd2633d30e1d
yhw991228 <yhw991228@gmail.com>
2024-05-16 12:15:01 +0900

    ✨ feat: menu page for parent
    Change-Id: I8f5312a66c07c65754d3b4055866ecff23564bae

---
+++ frontend/src/app/(parent)/manage/store/menu/page.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import axios from '@/apis/axios';
+import StoreCategory from '@/components/store/StoreCategory';
+import { PointAmount } from '@/types/point';
+import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import Link from 'next/link';
+import { css } from 'styled-system/css';
+import { Stack } from 'styled-system/jsx';
+
+const Category = () => {
+  const categories: {
+    type: 'toy' | 'book' | 'stationery' | 'convenienceStore' | 'iceCream';
+    name: string;
+  }[] = [
+    { type: 'toy', name: '장난감/인형' },
+    { type: 'book', name: '도서/동화책' },
+    { type: 'stationery', name: '문구류' },
+    { type: 'convenienceStore', name: '편의점' },
+    { type: 'iceCream', name: '음료/아이스크림' },
+  ];
+
+  const getPoint = async () => {
+    return await axios.get<PointAmount>('/point/amount').then((res) => {
+      return res.data.data;
+    });
+  };
+
+  const amount = useQuery({
+    queryKey: ['point'],
+    queryFn: getPoint,
+  });
+
+  const defaultBrand = ['드림아트', '', '', 'GS25', ''];
+  return (
+    <>
+      <header
+        className={css({
+          display: 'flex',
+          position: 'sticky',
+          height: '2.5rem',
+          top: 0,
+          bg: 'background.normal.normal/80',
+          backdropFilter: 'auto',
+          backdropBlur: 'sm',
+          px: '1rem',
+          justifyContent: 'space-between',
+          alignItems: 'end',
+        })}
+      >
+        <Link
+          className={css({
+            fontFamily: 'yeoljeong',
+            fontSize: '28px',
+            lineHeight: '38.02px',
+            color: 'label.alternative',
+          })}
+          href={'/'}
+        >
+          habitz
+        </Link>
+        <span
+          className={css({
+            display: 'flex',
+            textStyle: 'headline1.bold',
+            gap: '0.125rem',
+            py: '0.25rem',
+          })}
+        >
+          {amount.data?.point || 0}
+          <Image src="/coin.svg" alt="coin" width={18} height={18} />
+        </span>
+      </header>
+      <div
+        className={css({
+          display: 'flex',
+          w: 'full',
+          flexDir: 'column',
+          gap: '1rem',
+          p: '1rem',
+        })}
+      >
+        <Stack
+          h={'100%'}
+          justify={'space-between'}
+          display="flex"
+          flexDir="column"
+          gap="1.25rem"
+        >
+          {categories.map((category, id) => (
+            <Link
+              key={id}
+              href={{
+                pathname: '/manage/store/list',
+                query: { category: category.name, brand: defaultBrand[id] },
+              }}
+            >
+              <StoreCategory
+                type={category.type}
+                name={category.name}
+              ></StoreCategory>
+            </Link>
+          ))}
+        </Stack>
+      </div>
+    </>
+  );
+};
+export default Category;
