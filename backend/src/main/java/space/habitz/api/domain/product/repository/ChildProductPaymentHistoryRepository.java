d917042fe6241cf20c8cfcf59097c0babe985475
용희원 <yhw991228@gmail.com>
2024-05-17 16:40:35 +0900

    ✏️ fix: purchase history fix
    Change-Id: Ibb61e9cf2aaf7b40532b96386df77960eb81aafe

--- backend/src/main/java/space/habitz/api/domain/product/repository/ChildProductPaymentHistoryRepository.java
+++ backend/src/main/java/space/habitz/api/domain/product/repository/ChildProductPaymentHistoryRepository.java
@@ -1,5 +1,7 @@
 package space.habitz.api.domain.product.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import space.habitz.api.domain.product.entity.ChildProductPaymentHistory;
 @Repository
 public interface ChildProductPaymentHistoryRepository
 	extends JpaRepository<ChildProductPaymentHistory, Long>, ChildProductPaymentHistoryCustomRepository {
+
+	Optional<ChildProductPaymentHistory> findChildProductPaymentHistoryById(Long id);
 }
