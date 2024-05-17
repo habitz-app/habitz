7c9f349d964608b67faad605e4ef1252133d8319
sejeong-park <tpwjd29@gmail.com>
2024-05-17 14:27:52 +0900

    :recycle: refator: change memberProfileDto and change search mission return type
    - memberProfileDto에 이미지와 닉네임 추가합니다.
    - 조회 API에 approval 프로필을 추가합니다.
    - 인증 없이 승인 / 거절 시 이슈 있던거 수정합니다.

--- backend/src/main/java/space/habitz/api/domain/mission/dto/MissionRecognitionDto.java
+++ backend/src/main/java/space/habitz/api/domain/mission/dto/MissionRecognitionDto.java
@@ -12,6 +12,9 @@ public record MissionRecognitionDto(
 	LocalDateTime updatedAt
 ) {
 	public static MissionRecognitionDto of(MissionRecognition missionRecognition) {
+		if (missionRecognition == null) {
+			return null;
+		}
 		// mission 조회 시, 존재하는 인증 정보를 가져온다.
 		return MissionRecognitionDto.builder()
 			.image(missionRecognition.getImage())
