b6a6cabd7fd2708c77a63e844f4ef94ad386eafa
조승우 <kjsw12@naver.com>
2024-05-18 18:09:32 +0900

    🔥 fix: 알림 기능 Member Lazy Loading 관련 문제 해결
    Change-Id: I80034c88e968ccc5478ac20fe072bd0f3caa3d61

--- backend/src/main/java/space/habitz/api/domain/notification/repository/NotificationRepository.java
+++ backend/src/main/java/space/habitz/api/domain/notification/repository/NotificationRepository.java
@@ -3,12 +3,14 @@ package space.habitz.api.domain.notification.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import space.habitz.api.domain.member.entity.Member;
 import space.habitz.api.domain.notification.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	@EntityGraph(attributePaths = {"member"})
 	List<Notification> findAllByMemberAndDeletedAtIsNull(Member member);
 
 	Optional<Notification> findByIdAndDeletedAtIsNull(Long id);
