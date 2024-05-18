392d1ec5131b9f802075ee413431f5fae3c26fc9
조승우 <kjsw12@naver.com>
2024-05-18 19:07:02 +0900

    🔥 fix: 알림 DTO 일부 수정
    Change-Id: I91218ff41aaf3e86009ba9ebafe926c9f59743ed

--- backend/src/main/java/space/habitz/api/domain/notification/dto/NotificationResponseDto.java
+++ backend/src/main/java/space/habitz/api/domain/notification/dto/NotificationResponseDto.java
@@ -1,5 +1,7 @@
 package space.habitz.api.domain.notification.dto;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,16 +21,21 @@ public class NotificationResponseDto {
 	private String content;
 	private String memberUuid;
 	private boolean isRead;
+	private LocalDateTime createdAt;
+	private String href;
 
 	public static NotificationResponseDto convertTo(Notification notification) {
 		return NotificationResponseDto.builder()
 			.notificationId(notification.getId())
+			.type(notification.getType())
 			.title(notification.getTitle())
 			.content(notification.getContent())
 			.memberId(notification.getMember().getId())
 			.memberUuid(notification.getMember().getUuid())
 			.type(notification.getType())
-			.isRead(notification.getReadAt() == null)
+			.isRead(notification.getReadAt() != null)
+			.createdAt(notification.getCreatedAt())
+			.href(notification.getHref())
 			.build();
 	}
 }
