ceff66902993f996c4bc5ee482f34c4ff18c733c
sejeong-park <tpwjd29@gmail.com>
2024-05-19 19:56:15 +0900

    ♻️ refactor: add notification to member list
    - 특정 다수에게 알림을 보내는 노티피케이션 이벤트 생성
    
    Change-Id: I0431208fa00e17d6157454cc7bfc7b0e406fc796

--- backend/src/main/java/space/habitz/api/domain/notification/service/NotificationService.java
+++ backend/src/main/java/space/habitz/api/domain/notification/service/NotificationService.java
@@ -3,16 +3,15 @@ package space.habitz.api.domain.notification.service;
 import java.util.List;
 
 import space.habitz.api.domain.member.entity.Member;
-import space.habitz.api.domain.notification.dto.FamilyNotificationEvent;
-import space.habitz.api.domain.notification.dto.NotificationResponseDto;
-import space.habitz.api.domain.notification.dto.ParentNotificationEvent;
-import space.habitz.api.domain.notification.dto.SingleNotificationEvent;
+import space.habitz.api.domain.notification.dto.*;
 import space.habitz.api.domain.notification.entity.Notification;
 
 public interface NotificationService {
 	List<Notification> createFamilyNotification(FamilyNotificationEvent familyNotificationEvent);
 
 	List<Notification> createParentNotification(ParentNotificationEvent parentNotificationEvent);
+
+	List<Notification> createMultiNotification(MultiNotificationEvent multiNotificationEvent);
 
 	Notification createSingleNotification(SingleNotificationEvent singleNotificationEvent);
 
